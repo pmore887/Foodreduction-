47e1e71be40270a1491b50c89b4fd7b89c5a636e
Pranali More <pranalimore612@gmail.com>
2026-09-26 00:34:02 +0530

    Fix import path for AuthContext in main.jsx

--- main.jsx
+++ main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import { AuthProvider, useAuth } from './lib/AuthContext.jsx'
+import { AuthProvider, useAuth } from './AuthContext.jsx'
 import Layout from './components/Layout.jsx'
 import Login from './pages/Login.jsx'
 import Register from './pages/Register.jsx'
